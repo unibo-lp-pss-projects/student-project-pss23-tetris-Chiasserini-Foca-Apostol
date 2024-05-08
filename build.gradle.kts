8b4facd3f8ff2da962130e2d5475571d90f5d85d
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-08 22:13:07 +0200

    Add checkstyle plugin and his configuration file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,13 @@
-plugins { java }
+plugins {
+    java
+    application
+    checkstyle
+}
+
+repositories {
+    mavenCentral() // Config for download Mavan dependencies
+}
+
+application { // Config for specify the main class 
+    mainClass.set("main.java.it.unibo.tetris.Main")
+}
