849265ff02c7cb0b2bf718def71e3da926804d0e
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-08 22:24:09 +0200

    Set initial properties of GamePanel

--- src/main/java/it/unibo/tetris/GamePanel.java
+++ src/main/java/it/unibo/tetris/GamePanel.java
@@ -1,5 +1,20 @@
 package main.java.it.unibo.tetris;
 
-public class GamePanel {
+import java.awt.Dimension;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
     
+    //Dimensions of the window 
+    public static final int WIDTH = 840;
+    public static final int HEIGHT = 900;
+
+    public GamePanel () {
+
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+    }
 }
