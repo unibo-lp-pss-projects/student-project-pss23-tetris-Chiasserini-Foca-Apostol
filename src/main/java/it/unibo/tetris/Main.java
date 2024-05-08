bb6d567982f518288ea8b6aaaed86a407922bc49
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-08 22:16:38 +0200

    Create JFrame

--- src/main/java/it/unibo/tetris/Main.java
+++ src/main/java/it/unibo/tetris/Main.java
@@ -1,9 +1,17 @@
 package main.java.it.unibo.tetris;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        //Create JFrame and set it visible
+        JFrame gameWindow = new JFrame("Tetris"); 
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setResizable(false);
+        gameWindow.pack();
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
     }
     
 }
