5c9af803e14391fd19f9096b347bc8b50a228b2e
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-08 22:48:20 +0200

    Implement methods to draw the Playarea

--- src/main/java/it/unibo/tetris/PlayManager.java
+++ src/main/java/it/unibo/tetris/PlayManager.java
@@ -1,5 +1,42 @@
 package main.java.it.unibo.tetris;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 public class PlayManager {
+        //border thickness
+        private static final int BORDER = 4;
+
+        //hypotetical height of the written title 
+        public static final int TITLE_HEIGHT = 200;
     
+        //Playarea measures
+        final int WIDTH = 360;
+        final int HEIGHT = 600;
+    
+        //Playarea bounds
+        public static int left_x;
+        public static int right_x;
+        public static int top_y;
+        public static int bottom_y;
+    
+        public PlayManager() {
+            //Set Playarea dimensions
+            left_x = 50;
+            right_x = left_x + WIDTH;
+            top_y = 50;
+            bottom_y = top_y + HEIGHT;
+        }
+    
+        public void update() {
+            //TO DO
+        }
+    
+        public void draw(Graphics2D g2) {
+            //Draw the PlayArea.        
+            g2.setColor(Color.white);
+            g2.setStroke(new BasicStroke(4f));
+            g2.drawRoundRect(left_x - BORDER, top_y + TITLE_HEIGHT - BORDER, WIDTH + BORDER * 2, HEIGHT + BORDER * 2, 25, 25);
+        }
 }
