c9b7fd6aec06b8adfd4d3239f00d676fb5ac142b
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-09 11:22:59 +0200

    Add unimplemented methods

--- src/main/java/it/unibo/tetris/mino/Mino_Impl.java
+++ src/main/java/it/unibo/tetris/mino/Mino_Impl.java
@@ -1,11 +1,12 @@
 package main.java.it.unibo.tetris.mino;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 import main.java.it.unibo.tetris.mino.api.Block;
 import main.java.it.unibo.tetris.mino.api.Mino;
 
 public class Mino_Impl implements Mino {
-
-    public class Mino_Impl implements Mino {
     private static final int NUM_OF_BLOCK_OF_A_MINO = 4;
     private static final int STARTING_DIRECTION = 1;
     // Create and instantiate 2 array of Block
@@ -24,5 +25,50 @@ public class Mino_Impl implements Mino {
             bottomCollision;
     public boolean active = true, deactivating;
     
-}
+
+
+    @Override
+    public void create(Color c) {
+        // TO DO
+    }
+
+    @Override
+    public void setXY(int x, int y) {
+        // TO DO
+    }
+
+    @Override
+    public void updateXY(int direction) {
+        // TO DO
+    }
+
+    @Override
+    public void getNextDirection(int direction) {
+        // TO DO
+    }
+
+    @Override
+    public void checkMovementCollision() {
+        // TO DO
+    }
+
+    @Override
+    public void checkRotationCollision() {
+        // TO DO
+    }
+
+    @Override
+    public void checkStaticBlockCollision() {
+        // TO DO
+    }
+
+    @Override
+    public void update() {
+        // TO DO
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        // TO DO
+    }
 }
