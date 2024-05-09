389f4dac4d54ff6ea7569d60ce42900f82a5140f
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-09 10:48:05 +0200

    Creating constructor of "Block"

--- src/main/java/it/unibo/tetris/mino/api/Block.java
+++ src/main/java/it/unibo/tetris/mino/api/Block.java
@@ -1,6 +1,7 @@
 package main.java.it.unibo.tetris.mino.api;
 
 import java.awt.Rectangle;
+import java.awt.Color;
 
 public class Block extends Rectangle {
     
@@ -9,4 +10,15 @@ public class Block extends Rectangle {
 
     // Constant for the size of the block
     public static final int SIZE = 30;
+
+    // Coordinates ot the block
+    public int x, y;
+
+    public Color c;
+
+    // Create Constructor of "Block"
+    public Block(Color c) { 
+        this.c = c;
+    }
+
 }
