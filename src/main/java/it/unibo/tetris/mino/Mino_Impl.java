bc0a3d7ee13e56cd0ae68d8789d9960ec20d6f94
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-09 11:14:34 +0200

    Create "Mino_impl" that implements Mino

---
+++ src/main/java/it/unibo/tetris/mino/Mino_Impl.java
@@ -0,0 +1,16 @@
+package main.java.it.unibo.tetris.mino;
+
+import main.java.it.unibo.tetris.mino.api.Block;
+import main.java.it.unibo.tetris.mino.api.Mino;
+
+public class Mino_Impl implements Mino {
+
+    public class Mino_Impl implements Mino {
+    private static final int NUM_OF_BLOCK_OF_A_MINO = 4;
+    private static final int STARTING_DIRECTION = 1;
+    // Create and instantiate 2 array of Block
+    public Block b[] = new Block[NUM_OF_BLOCK_OF_A_MINO];
+    public Block tempB[] = new Block[NUM_OF_BLOCK_OF_A_MINO];
+    
+}
+}
