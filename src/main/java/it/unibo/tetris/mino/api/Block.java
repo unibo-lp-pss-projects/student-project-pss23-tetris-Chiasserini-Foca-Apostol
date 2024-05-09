a385b15c1bbcb05541e71d42997729ee55c4bf93
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-09 10:45:22 +0200

    Add size and margin constants of the block

--- src/main/java/it/unibo/tetris/mino/api/Block.java
+++ src/main/java/it/unibo/tetris/mino/api/Block.java
@@ -4,4 +4,9 @@ import java.awt.Rectangle;
 
 public class Block extends Rectangle {
     
+    // Constant for the margin of the block
+    public static final int MARGIN = 2;
+
+    // Constant for the size of the block
+    public static final int SIZE = 30;
 }
