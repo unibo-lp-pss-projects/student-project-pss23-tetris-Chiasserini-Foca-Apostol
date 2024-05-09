831eed7abf9d779edde326ee267a8094e2b1f4f2
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-09 10:17:01 +0200

    Comments that follows Javadoc guidelines

--- src/main/java/it/unibo/tetris/Main.java
+++ src/main/java/it/unibo/tetris/Main.java
@@ -2,15 +2,27 @@ package main.java.it.unibo.tetris;
 
 import javax.swing.JFrame;
 
+/**
+ * Class {@link Main}, the main of the project
+ */
 public class Main {
 
+    /**
+     * The main method.
+     *
+     * @param args The {@link String}[] args.
+     */
     public static void main(String[] args) {
-        //Create JFrame and set it visible
+        /*
+         * Create JFrame and set it visible.
+         */
         JFrame gameWindow = new JFrame("Tetris"); 
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setResizable(false);        
 
-        //add GamePanel to the gameWindow
+        /*
+         * Add GamePanel to the gameWindow.
+         */
         GamePanel gp = new GamePanel();
         gameWindow.add(gp);
         gameWindow.pack();
