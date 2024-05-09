1715d3468b6b2402d35475990cde694606f26fb4
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-09 10:57:43 +0200

    Set coordinations and update them with "setXY" and "updateXY"

--- src/main/java/it/unibo/tetris/mino/api/Mino.java
+++ src/main/java/it/unibo/tetris/mino/api/Mino.java
@@ -3,5 +3,15 @@ package main.java.it.unibo.tetris.mino.api;
 import java.awt.Color;
 
 public interface Mino {
+
+    // Create a new mino with a given color
+    public void create(Color c);
+
+    // Sets the x and y coordinates of the mino
+    public void setXY(int x, int y);
+
+    // Updates the x and y coordinates of the mino based on the given direction.
+    public void updateXY(int direction);
+
     
 }
