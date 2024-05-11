310c040b4a16802481cfc91360c91c884376fb5a
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-11 12:00:12 +0200

    Set coordinates of the "Square"

--- src/main/java/it/unibo/tetris/mino/Mino_Square.java
+++ src/main/java/it/unibo/tetris/mino/Mino_Square.java
@@ -12,4 +12,20 @@ public class Mino_Square extends Mino_Impl {
     public Mino_Square() {
         create(Color.yellow);
     }
+
+    // Set coordinates of "Square"
+    public void setXY(int x, int y) {
+        //
+        // ° °
+        // ° °
+        b[0].x = x;
+        b[0].y = y;
+        b[1].x = b[0].x;
+        b[1].y = b[0].y + Block.SIZE;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
+        b[3].x = b[0].x + Block.SIZE;
+        b[3].y = b[0].y + Block.SIZE;
+
+    }
 }
