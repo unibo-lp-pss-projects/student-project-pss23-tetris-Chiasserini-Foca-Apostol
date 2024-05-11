99352493350bde4d4479b672ca4c179ba5965874
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-11 11:38:48 +0200

    Set coordinates of the Bar

--- src/main/java/it/unibo/tetris/mino/Mino_Bar.java
+++ src/main/java/it/unibo/tetris/mino/Mino_Bar.java
@@ -2,11 +2,32 @@ package main.java.it.unibo.tetris.mino;
 
 import java.awt.Color;
 
+import it.unibo.tetris.mino.api.Block;
 import main.java.it.unibo.tetris.mino.api.*;
 
 
 
-
+// Class for "Bar"
 public class Mino_Bar extends Mino_Impl {
+
+    // Constructor for Mino_Bar
+    public Mino_Bar() {
+        create(Color.cyan);
+    }
     
+    // Coordinates of the "Bar"
+    public void setXY(int x, int y) {
+        //
+        // ° ° ° °
+        //
+        b[0].x = x;
+        b[0].y = y;
+        b[1].x = b[0].x - Block.SIZE;
+        b[1].y = b[0].y;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
+        b[3].x = b[0].x + Block.SIZE * 2;
+        b[3].y = b[0].y;
+
+    }
 }
