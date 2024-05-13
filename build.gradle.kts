d25b2f89cb8af5c4338caefe4b9a94d47cd47be6
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-13 22:38:29 +0200

    Add JunitTest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,18 @@ repositories {
     mavenCentral() // Config for download Mavan dependencies
 }
 
+dependencies {
+    // Dependencies for write the JUnitTest
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    // Dependencies for run the JUnitTest
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+}
+
+tasks.test {
+    useJUnitPlatform() // Configure task "test" for use JUnit Platform
+    testLogging { events("passed", "skipped", "failed") } // Print Test's result
+}
+
 application { // Config for specify the main class 
     mainClass.set("main.java.it.unibo.tetris.Main")
 }
