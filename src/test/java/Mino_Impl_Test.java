3c809349360cbf882af33c02da81b7a90b303807
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-13 22:47:54 +0200

    Implement BeforeEach

--- src/test/java/Mino_Impl_Test.java
+++ src/test/java/Mino_Impl_Test.java
@@ -1,4 +1,17 @@
-public class Mino_Impl_Test {
+import org.junit.jupiter.api.BeforeEach;
 
+import java.awt.Color;
+
+import it.unibo.tetris.mino.Mino_Impl;
+
+public class Mino_Impl_Test {
+     private Mino_Impl mino;
+
+    @BeforeEach
+    public void setUp() {
+        mino = new Mino_Impl();
+        //Color is not change the result of the tests.
+        mino.create(Color.RED);
+    }
     
 }
