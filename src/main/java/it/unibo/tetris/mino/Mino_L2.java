b512e35517af6044969ca3a04d40635e6543ccfc
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-13 12:11:58 +0200

    Add first 2 directions

--- src/main/java/it/unibo/tetris/mino/Mino_L2.java
+++ src/main/java/it/unibo/tetris/mino/Mino_L2.java
@@ -26,5 +26,57 @@ public class Mino_L2 extends Mino_Impl{
         b[3].x = b[0].x - Block.SIZE;
         b[3].y = b[0].y + Block.SIZE;
     }
-    
+
+    // Determines next direction
+    public void getNextDirection(int direction) {
+
+        switch (direction) {
+            case 1:
+                getDirection2();
+                break;
+            case 2:
+                getDirection3();
+                break;
+            case 3:
+                getDirection4();
+                break;
+            case 4:
+                getDirection1();
+                break;
+        }
+    }
+
+    // First one
+    private void getDirection1() {
+        //   °
+        //   °
+        // ° °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(1);
+    }
+
+    // Second one
+    private void getDirection2() {
+        //
+        // °
+        // ° ° °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(2);
+    }
 }
