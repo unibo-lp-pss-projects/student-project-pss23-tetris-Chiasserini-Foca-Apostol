59498b5395ace1266d6be1397e9ad11d5a3d3a9f
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-13 11:53:22 +0200

    Add 2 last directions of the "Mino_L1"

--- src/main/java/it/unibo/tetris/mino/Mino_L1.java
+++ src/main/java/it/unibo/tetris/mino/Mino_L1.java
@@ -80,5 +80,39 @@ public class Mino_L1 {
 
         updateXY(2);
     }
+
+    // Thirs direction
+    private void getDirection3() {
+        // ° °
+        //   °
+        //   °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(3);
+    }
+
+    // Last direction
+    private void getDirection4() {
+        //
+        //     °
+        // ° ° °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(4);
+    }
     
 }
