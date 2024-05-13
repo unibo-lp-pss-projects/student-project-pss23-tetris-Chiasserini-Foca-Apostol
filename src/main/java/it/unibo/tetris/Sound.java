f6644b859b0ca9dadc7745a9e2f50a9acec9db14
tomm1341 <tommaso.foca@studio.unibo.it>
2024-05-13 16:13:30 +0200

    In Sound.java created an array that contains the sounds

--- src/main/java/it/unibo/tetris/Sound.java
+++ src/main/java/it/unibo/tetris/Sound.java
@@ -3,8 +3,25 @@ package main.java.it.unibo.tetris;
 import java.net.URL;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
+// This class manages the sounds of the game
 
 public class Sound {
     
+    //Variable that manages sounds
+    private Clip musicClip;
+
+    //Private array for storaging sounds
+    private URL sounds[] = new URL[10];
+
+    //Fill array with soundtracks
+
+    public Sound() {
+        sounds[0] = getClass().getResource("/Tetris.wav");
+        sounds[1] = getClass().getResource("/delete line.wav");
+        sounds[2] = getClass().getResource("/gameover.wav");
+        sounds[3] = getClass().getResource("/rotation.wav");
+        sounds[4] = getClass().getResource("/touch floor.wav");
+    }
 }
