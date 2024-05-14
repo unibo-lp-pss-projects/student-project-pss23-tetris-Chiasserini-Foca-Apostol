4f522426d2771375c5275fb1f15e5a62d717f3fc
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-14 12:21:35 +0200

    Last 2 directions of Mino_T

--- src/main/java/it/unibo/tetris/mino/Mino_T.java
+++ src/main/java/it/unibo/tetris/mino/Mino_T.java
@@ -81,5 +81,39 @@ public class Mino_T extends Mino_Impl{
 
         updateXY(2);
     }
+
+    // Third direction
+    private void getDirection3() {
+        //   °
+        // ° °
+        //   °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y;
+
+        updateXY(3);
+    }
+
+    // Last direction
+    private void getDirection4() {
+        //
+        //   °
+        // ° ° °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(4);
+    }
     
 }
