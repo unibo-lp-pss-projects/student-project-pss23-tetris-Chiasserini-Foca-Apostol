ceef810fcda834cd4bd441a03297a50bce0e69f6
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-14 12:17:25 +0200

    First 2 directions of Mino_T

--- src/main/java/it/unibo/tetris/mino/Mino_T.java
+++ src/main/java/it/unibo/tetris/mino/Mino_T.java
@@ -47,5 +47,39 @@ public class Mino_T extends Mino_Impl{
         }
 
     }
+
+    // First one
+    private void getDirection1() {
+        // °
+        // ° °
+        // °  
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y;
+
+        updateXY(1);
+    }
+
+    // Second one
+    private void getDirection2() {
+        //
+        // ° ° °
+        //   °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(2);
+    }
     
 }
