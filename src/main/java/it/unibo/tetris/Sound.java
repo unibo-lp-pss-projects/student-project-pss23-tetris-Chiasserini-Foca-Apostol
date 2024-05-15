10c6a44c4ba9b613bcc0cbbcb7d61cdd6304133e
tomm1341 <tommaso.foca@studio.unibo.it>
2024-05-15 16:19:58 +0200

    In Sound.java comments now follow Javadoc guidelines

--- src/main/java/it/unibo/tetris/Sound.java
+++ src/main/java/it/unibo/tetris/Sound.java
@@ -3,18 +3,26 @@ package main.java.it.unibo.tetris;
 import java.net.URL;
 import javax.sound.sampled.*;
 
-// This class manages the sounds of the game
-
+/**
+ * Class {@link Sound} that manage the sounds of the game.
+ */
 public class Sound {
     
-    //Variable that manages sounds
+    /**
+     * Private {@link Clip} variable
+     * for manage sounds.
+     */
     private Clip musicClip;
 
-    //Private array for storaging sounds
+    /**
+     * Private {@link URL} array
+     * for storage sounds.
+     */
     private URL sounds[] = new URL[10];
 
-    //Fill array with soundtracks
-
+    /**
+     * Fill {@link URL} array with soundtracks
+     */
     public Sound() {
         sounds[0] = getClass().getResource("/Tetris.wav");
         sounds[1] = getClass().getResource("/delete line.wav");
@@ -23,7 +31,12 @@ public class Sound {
         sounds[4] = getClass().getResource("/touch floor.wav");
     }
 
-    //Method that plays a specified sound
+    /**
+     * Method that plays a specified sound.
+     * 
+     * @param soundIndex index of the sound
+     * @param music boolean for play the sound 
+     */
     public void play(int soundIndex, boolean music){
         
         try {
@@ -34,7 +47,9 @@ public class Sound {
                 musicClip = clip;
             }
 
-            //Behaviour of the class when the sound finishes playing
+            /*
+             * Behaviour of the class when the sound finishes playing.
+             */
             clip.open(ais);
             clip.addLineListener(new LineListener() {
                 @Override
@@ -51,21 +66,27 @@ public class Sound {
         }
     }
 
-    //Method for playing a sound in loop mode
-
+    /**
+     * Method for playing a sound in looping mode.
+     */
     public void loop() {
         musicClip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
-    //Stop the sound
-
+    /**
+     * Stop the sound that is currently playing
+     */
     public void stop() {
         musicClip.stop();
         musicClip.close();
     }
 
-    //Set the volume
-
+    /**
+     * Set a specified volume value. 
+     * range in db (-80.0 - 6.0).
+     * 
+     * @param volume float value of the volume.
+     */
     public void setVolume(float volume) {
         if (musicClip != null) {
             FloatControl control = (FloatControl)musicClip.getControl(FloatControl.Type.MASTER_GAIN);
