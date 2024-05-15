cd2cf4518cf4fb7346e610b9f9564f73385808d7
tomm1341 <tommaso.foca@studio.unibo.it>
2024-05-15 16:40:15 +0200

    Created Menu.java and its imports

---
+++ src/main/java/it/unibo/tetris/Menu.java
@@ -0,0 +1,17 @@
+package main.java.it.unibo.tetris;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Menu extends JPanel{
+    
+    private JPanel mainPanel;
+    private JFrame frame;
+
+
+    public Menu() {
+
+        //Create the JFrame
+
+    }
+}
