a1332ce37e84a36d9bbdf18b8bbc2bcb18aac2d7
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-16 13:26:27 +0200

    Implement BeforeEach

--- src/test/java/Mino_L2_Test.java
+++ src/test/java/Mino_L2_Test.java
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.tetris.PlayManager;
+import it.unibo.tetris.mino.Mino_L2;
+
 public class Mino_L2_Test {
-    
+    private Mino_L2 mino;
+
+    @BeforeEach
+    public void setUp() {
+        mino = new Mino_L2();
+        PlayManager playManager = new PlayManager();
+    }
 }
