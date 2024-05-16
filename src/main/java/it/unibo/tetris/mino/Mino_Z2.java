8984fef5bc8d239fc68acc4b889ca2fbf2e64f9e
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-16 12:37:26 +0200

    Add the directions of the Mino_Z2

--- src/main/java/it/unibo/tetris/mino/Mino_Z2.java
+++ src/main/java/it/unibo/tetris/mino/Mino_Z2.java
@@ -38,5 +38,39 @@ public class Mino_Z2 extends Mino_Impl{
                 break;
         }
     }
+
+    // First direction
+    private void getDirection1() {
+        // °
+        // ° °
+        //   °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(1);
+    }
+
+    // Second direction
+    private void getDirection2() {
+        //
+        //   ° °
+        // ° °
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(2);
+    }
     
 }
