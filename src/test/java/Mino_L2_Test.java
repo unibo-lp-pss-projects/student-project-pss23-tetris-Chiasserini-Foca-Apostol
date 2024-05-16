9222c6461f59eb9533f04a4817599d932e741d3e
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2024-05-16 13:30:29 +0200

    testDirection1 method

--- src/test/java/Mino_L2_Test.java
+++ src/test/java/Mino_L2_Test.java
@@ -35,4 +35,29 @@ public class Mino_L2_Test {
         assertEquals(mino.b[3].x, mino.b[0].x - Block.SIZE);
         assertEquals(mino.b[3].y, mino.b[0].y + Block.SIZE);
     }
+
+    @Test
+    public void testDirection1() {
+        /**
+         * Set initial position
+         */
+        mino.setXY(PlayManager.MINO_START_X, PlayManager.MINO_START_Y);
+        
+        /**
+         *  Call getNExtDirection with starting direction 1
+         */ 
+        mino.getNextDirection(1);
+
+        /**
+         * Verify the position of all blocks
+         */
+        assertEquals(mino.b[0].x, PlayManager.MINO_START_X);
+        assertEquals(mino.b[0].y, PlayManager.MINO_START_Y);
+        assertEquals(mino.b[1].x, mino.b[0].x + Block.SIZE);
+        assertEquals(mino.b[1].y, mino.b[0].y);
+        assertEquals(mino.b[2].x, mino.b[0].x - Block.SIZE);
+        assertEquals(mino.b[2].y, mino.b[0].y);
+        assertEquals(mino.b[3].x, mino.b[0].x - Block.SIZE);
+        assertEquals(mino.b[3].y, mino.b[0].y - Block.SIZE);
+   }
 }
