aeed4053783b1a8ce3f1d386f58e339db368329e
tomm1341 <tommaso.foca@studio.unibo.it>
2024-05-21 15:26:15 +0200

    Added back-button and tutorial imgs and imported them in Tutorial.java

--- src/main/java/it/unibo/tetris/Tutorial.java
+++ src/main/java/it/unibo/tetris/Tutorial.java
@@ -4,5 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Tutorial extends JPanel{
+    private Image mainImage;
+    private ImageIcon icon = new ImageIcon("src/res/img/tutorial.png");
+    // Button credits: Freepik.com
+    private ImageIcon backButtonImage = new ImageIcon("src/res/img/back-button.png");
+
     
 }
